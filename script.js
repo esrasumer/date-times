329f08fc7ff9a26d626f2ac50dd23a6ea2620c39
esrasumer <esrasumer28@gmail.com>
2022-05-15 23:30:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+/*let d = new Date();
+console.log(d);
+console.log(d.getDate());
+console.log(d.getMonth() + 1);
+console.log(d.getFullYear());
+
+let date = new Date('8/24/2022 00:08:45');
+console.log(date)
+
+let dateB = new Date(2022, 7, 24, 00, 08, 45); //aya 7 verdik 8. ay çıktı neden?
+console.log(dateB);
+// 1/1/1990 tarihinden bir önceki günü yazadık
+let d1 = new Date('1 / 1 / 1990');
+let dayOfMounth = d1.getDate();
+d1.setDate(dayOfMounth - 1);
+console.log(d1);
+
+// iki tarih arasında geçen zaman
+
+let start = new Date('1/1/1990');
+let end = new Date('1/1/1991');
+
+let milisecond = end - start;
+let saniye = milisecond / 1000;
+let dakika = saniye / 60;
+let saat = dakika / 60;
+let gun = saat / 24;
+
+console.log('milisecond :' + milisecond);
+console.log('saniye :' + saniye);
+console.log('dakika :' + dakika);
+console.log('saat :' + saat);
+console.log('gun :' + gun);
+
+
+//yaş hesaplama 
+let today = new Date();
+let brithday2 = new Date(2001, 3, 21);
+
+console.log(today.getFullYear() - brithday2.getFullYear());*/
